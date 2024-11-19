fb3ac2ed8cbaf6768780f92fb75fd7449e706833
jesusrafaell <jesusrafaellh@gmail.com>
2024-11-18 21:46:41 -0400

    add multi client

--- app/main.py
+++ app/main.py
@@ -1,22 +1,29 @@
 import socket
+import _thread
+
+def client_handler(conn: socket): 
+    response = "+PONG\r\n".encode()
+    conn.send(response)
+    conn.close()
+
+def accept_connectins(server_socket):
+    conn, addr = server_socket.accept()
+    print(f"Connected to: {addr}")
+    _thread.start_new_thread(client_handler, ( conn,))
 
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
 
-    while True:
-        print(f"Connected {addr}")
-
-        data = conn.recv(1024)
-        if not data:
-            break
-        response = "+PONG\r\n".encode()
-        conn.sendall(response)
-
-    conn.close()
+    try:
+        while True:
+            accept_connectins(server_socket)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        server_socket.close()
 
 if __name__ == "__main__":
     main()
